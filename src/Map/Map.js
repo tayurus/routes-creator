ea31ced0036c10366e552d91d5b1d9bae1343d25
Daria Sminova <daria@realrussialimited.local>
2018-06-27 17:27:11 +0300

    add removable markers

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -1,37 +1,73 @@
 import React from "react";
 export class Map extends React.Component {
-
     constructor(props) {
         super(props);
         this.google = window.google;
-        this.map = {};
     }
 
     componentDidMount() {
+        console.log("componentDidMount");
         this.map = new this.google.maps.Map(document.getElementById("map"), {
             zoom: 7,
             center: { lat: 31.85, lng: -87.65 }
         });
+
     }
 
-
-    componentDidUpdate() {
-        let map = this.map;
+    shouldComponentUpdate() {
+        let map = new this.google.maps.Map(document.getElementById("map"), {
+            zoom: 7,
+            center: { lat: 31.85, lng: -87.65 }
+        });
         let google = this.google;
         var geocoder = new google.maps.Geocoder();
         if (this.props.points.length === 1) {
             /*create marker for point*/
             codeAddress(this.props.points[0]);
+        } else {
+            var directionsService = new google.maps.DirectionsService();
+
+            var directionsDisplay = new google.maps.DirectionsRenderer({
+                draggable: true,
+                map: map
+            });
+
+            let wayPoints = [];
+
+            for (let i = 1; i <= this.props.points.length - 1; i++) {
+                wayPoints.push({ location: this.props.points[i] });
+            }
+
+            directionsService.route(
+                {
+                    origin: this.props.points[0],
+                    waypoints: wayPoints,
+                    destination: this.props.points[
+                        this.props.points.length - 1
+                    ],
+                    travelMode: "DRIVING"
+                },
+                function(response, status) {
+                    if (status === "OK") {
+                        directionsDisplay.setDirections(response);
+                        console.log(map.markers);
+                    } else {
+                        window.alert(
+                            "Directions request failed due to " + status
+                        );
+                    }
+                }
+            );
         }
 
-         function codeAddress(str) {
+        function codeAddress(str) {
             function drawMarker(results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
                     var marker = new google.maps.Marker({
-                      position: results[0].geometry.location,
-                      map: map,
+                        position: results[0].geometry.location,
+                        map: map
                     });
-                    map.setCenter(results[0].geometry.location)
+                    map.setCenter(results[0].geometry.location);
                 }
             }
             geocoder.geocode({ address: str }, drawMarker);
@@ -99,24 +135,7 @@ export class Map extends React.Component {
         //     directionsService,
         //     directionsDisplay
         // ) {
-        //     directionsService.route(
-        //         {
-        //             origin: document.getElementById("start").value,
-        //             // waypoints: [{location: document.getElementById('waypoint').value}],
-        //             destination: document.getElementById("start").value,
-        //             travelMode: "DRIVING"
-        //         },
-        //         function(response, status) {
-        //             if (status === "OK") {
-        //                 directionsDisplay.setDirections(response);
-        //                 console.log(response);
-        //             } else {
-        //                 window.alert(
-        //                     "Directions request failed due to " + status
-        //                 );
-        //             }
-        //         }
-        //     );
+
         // }
         // initMap();
     }
